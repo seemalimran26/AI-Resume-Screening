1745f1bf39014858a81d558a17845a2fed63e705
seemalimran26 <seemalqureshi26@gmail.com>
2026-08-28 02:38:44 +0500

    url update

--- mern_ai/src/utils/axios.js
+++ mern_ai/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://independent-gentleness-production-6762.up.railway.app/",
+  baseURL: "smart-resume-screening.up.railway.app",
   withCredentials: true,
 });
 
