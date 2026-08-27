16cf738016e0305598e50feaa8950aecf2b1fd01
seemalimran26 <seemalqureshi26@gmail.com>
2026-08-28 02:27:42 +0500

    idChange

--- mern_ai/src/utils/firebase.jsx
+++ mern_ai/src/utils/firebase.jsx
@@ -1,15 +1,16 @@
-
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAazTU3sUcUMuyLVqLOAWczOdXB8HIetq4",
-  authDomain: "mernai-b1525.firebaseapp.com",
-  projectId: "mernai-b1525",
-  storageBucket: "mernai-b1525.firebasestorage.app",
-  messagingSenderId: "235206334441",
-  appId: "1:235206334441:web:7116ca0325274d99c01d78",
-  measurementId: "G-W98V3XNVJX"
+  apiKey: "AIzaSyB4zhwOW6Zu0WW8J4KcvFOhvn17BEE8YwM",
+  authDomain: "mernai-66a40.firebaseapp.com",
+  projectId: "mernai-66a40",
+  storageBucket: "mernai-66a40.firebasestorage.app",
+  messagingSenderId: "325025369777",
+  appId: "1:325025369777:web:e9547c2932374371eadc90",
+  measurementId: "G-1KX2C2V9Z9",
 };
 
 const app = initializeApp(firebaseConfig);
